5598c1d7fcb0cc38ef42a33abd671b8cab495930
Alejandro Di Battista <alejandrodibattista@gmail.com>
2020-07-21 01:21:15 -0300

    Update jumbo.rb

--- jumbo.rb
+++ jumbo.rb
@@ -8,14 +8,14 @@ require_relative 'utils'
 
 class Archivo
 	class << self
-		def leer(origen="productos.dsv")
+		def leer(origen = :datos)
 			origen = "#{origen}.dsv" if Symbol === origen
 			datos  = CSV.open(origen, :col_sep => "|")
 			campos = datos.shift
 			datos.map{|x| Hash(campos, x)}.normalizar
 		end
 
-		def escribir(datos, destino="datos.dsv")
+		def escribir(datos, destino = :datos)
 			destino = "#{destino}.dsv" if Symbol === destino
 			campos = datos.map(&:keys).uniq.flatten
 			CSV.open(destino, "wb", :col_sep => "|") do |csv|
@@ -54,8 +54,11 @@ end
 
 class Jumbo
 	class << self
+		URL = 'https://www.jumbo.com.ar/api/catalog_system/pub/category/tree/3'
+		URL_Imagenes = "https://jumboargentina.vteximg.com.br/arquivos/ids"
+
 		def clasificacion()
-			datos = JSON(URI.open('https://www.jumbo.com.ar/api/catalog_system/pub/category/tree/3').read)
+			datos = JSON(URI.open(URL).read)
 			datos.map do |d|
 				d["children"].map do |c|
 					if c["children"].size > 0
@@ -92,12 +95,12 @@ class Jumbo
 			productos
 		end
 		
-		def imagenes(productos,tamaño=512)
+		def imagenes(productos, destino='fotos', tamaño=512)
 			imagenes = productos.map{|x|x[:imagen]}
 
 			imagenes.each.with_index do |imagen, i|
-				origen  = "https://jumboargentina.vteximg.com.br/arquivos/ids/#{imagen}-#{tamaño}-#{tamaño}"
-				destino = "fotos/#{imagen}.jpg"
+				origen  = "#{URL_Imagenes}/#{imagen}-#{tamaño}-#{tamaño}"
+				destino = "#{destino}/#{imagen}.jpg"
 				unless File.exist?(destino)
 					print(".")
 					puts if i % 100 == 0
@@ -123,11 +126,77 @@ end
 
 class Tatito
 	class << self
+		URL = "http://tatito.com.ar/tienda/"
 
 		def clasificacion
-			page = Nokogiri::HTML(URI.open("http://tatito.com.ar/tienda/"))
+			page = Nokogiri::HTML(URI.open(URL))
 			page.css('#checkbox_15_2 option').map do |x|
-				{rubro: x.text.gsub("\u00A0"," ").gsub("\u00E9","é"), id: x["value"]}
+				rubro = x.text.gsub("\u00A0"," ").gsub("\u00E9","é").strip 
+				nivel = rubro[0..0] == "-" ? 1 : 0
+				url   = "?filters=product_cat[#{x["value"]}]"
+				{rubro: rubro.gsub(/^-\s*/,""), nivel: nivel, url: url }
+			end	
+		end
+	
+		def productos(clasificacion)
+			productos = []
+			clasificacion.each do |c|
+				url = "#{URL}/#{c[:url]}]"
+				page = Nokogiri::HTML(URI.open(:url))
+
+				page.css('.item_tienda').each do |x|
+					img = x.css("img").first
+					productos << {
+						nombre:  x.css(".titulo_producto a").text,
+						precio:  x.css(".amount").text.to_money,
+						imagen: img.nil? ? "" : img["src"].gsub(/-\d+x\d+\./,"."),
+						rubro: c[:rubro],
+						id: "%04i" % (productos.size + 1)
+						#marca:   x.css(".product-item__brand").text,
+						#link:    x.css(".product-item__name a").first["href"].split("/")[3],
+						#categoria:    c[:categoria],
+						#subcategoria: c[:subcategoria],
+					}
+					print "."
+				end
+				puts
+			end
+			productos.compact
+		end
+
+		def imagenes(imagenes, destino)
+			imagenes.each.with_index do |imagen, i|
+				origen  = imagen[:imagen]
+				destino = "#{destino}/#{imagen[:id]}.jpg"
+				unless origen.size == 0 || File.exist?(destino) 
+					print(".")
+					puts if i % 100 == 0
+					puts origen if ! Archivo.bajar(origen, destino)
+				end
+			end
+			puts "FIN"
+		end
+
+		def bajar_todo
+			puts "BAJANDO todos los datos de TATITO"
+			clasificacion = clasificacion()
+			Archivo.escribir(clasificacion, :clasificacion_tatito)
+			productos = Tatito.productos(clasificacion)
+			Archivo.escribir(productos, :tatito_productos)
+			Tatito.imagenes(productos, "fotos-tatito")
+			puts "FIN."
+		end
+	end
+end
+
+class Maxiconsumo
+	class << self
+		URL = "http://www.maxiconsumo.com/sucursal_capital/"
+
+		def clasificacion
+			page = Nokogiri::HTML(URI.open(URL))
+			page.css('#root li a').map do |x|
+				{rubro: x.text, nivel: x["data-level"].to_i, url: x["href"].gsub(URL,"") }
 			end	
 		end
 	
@@ -184,5 +253,7 @@ class Tatito
 end
 
 
-Tatito.bajar_todo
+# Tatito.bajar_todo
 # Jumbo.bajar_todo
+# pp Maxiconsumo.clasificacion()
+pp Tatito.clasificacion
