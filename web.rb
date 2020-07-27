86cd47a7f4d15c3701ba82ebe445b1f78bdda0fa
Alejandro Di Battista <alejandrodibattista@gmail.com>
2020-07-27 01:07:31 -0300

    Update web.rb

--- web.rb
+++ web.rb
@@ -334,13 +334,10 @@ class Maxiconsumo < Web
 	end
 end
 
-# if __FILE__ == $0 
-# 	Dir.chdir "C:/Users/Algacom/Documents/GitHub/catalogo/" do 
-# 		Jumbo.new.bajar_todo
-# 		Tatito.new.bajar_todo
-# 		Maxiconsumo.new.bajar_todo
-# 	end
-# end
-
-m = Maxiconsumo.new
-m.bajar_imagenes
+if __FILE__ == $0 
+	Dir.chdir "C:/Users/Algacom/Documents/GitHub/catalogo/" do 
+		Jumbo.new.bajar_todo
+		Tatito.new.bajar_todo
+		Maxiconsumo.new.bajar_todo
+	end
+end
