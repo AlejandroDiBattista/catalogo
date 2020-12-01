d932a3e06995ff8b36f76cc82b82197c67ef6976
Alejandro Di Battista <alejandrodibattista@gmail.com>
2020-12-01 01:39:59 -0300

    .

--- catalogo.rb
+++ catalogo.rb
@@ -198,6 +198,7 @@ class Catalogo
 	class << self 
 		def leer(base, posicion=0)
 			origen = listar(base, :productos)[posicion]
+        	puts ">> #{origen}"
         	tmp = new(base, Archivo.leer(origen))
         	tmp -= tmp.filtrar(&:error?)
 		end
