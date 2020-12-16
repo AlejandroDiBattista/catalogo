ba90b3f8d3b990a37bd2543a32943916644b1d7e
Alejandro Di Battista <alejandrodibattista@gmail.com>
2020-12-16 02:13:16 -0300

    .

--- cola.rb
+++ cola.rb
@@ -13,16 +13,8 @@ class Cola
     
     def initialize(nombre='Cola')
         self.nombre = nombre
-        self.reloj  = 0
+        self.reloj  = nil 
         self.registros = []
-    end
-
-    def avanzar(tiempo=1)
-        self.reloj += tiempo if tiempo > 0
-    end
-
-    def cantidad
-        registros.last ? registros.last.cantidad : 0
     end
 
     def entrar
@@ -33,66 +25,93 @@ class Cola
         registrar(-1)
     end
 
-    def registrar(cantidad)
-        cantidad += registros.last ? registros.last.cantidad : 0
-        self.registros << { cantidad: cantidad, hora: reloj } if cantidad >= 0
+    def configurando?
+        primero = entradas.first 
+        !primero || (self.hora - primero.hora >= TiempoMinimoObservacion)
     end
 
-    def permanentes
-        salida = []
-        anterior, *lista = self.registros
-        lista.each do |actual|
-            salida << anterior if actual.hora - anterior.hora >= TiempoMinimoAtencion
-            anterior = actual
-        end
-        salida << anterior if reloj - anterior.hora >= TiempoMinimoAtencion
-        salida
+    def editando?
+        ultimo = entradas.last 
+        !ultimo || (self.hora - ultimo.hora >= TiempoMinimoAtencion)
+    end
+
+    def hora 
+        self.reloj || Time.new
     end
 
     def entradas
-        salida = []
-        anterior, *lista = self.permanentes
-        lista.each do |actual|
-            salida << anterior if actual.hora - anterior.hora >= TiempoMinimoObservacion && salida.count == 0
-            salida << actual   if actual.cantidad < anterior.cantidad
-            anterior = actual
+        permanentes = []
+        reducir(self.registros) do | anterior, actual |
+            if actual 
+                permanentes << anterior if actual.hora - anterior.hora >= TiempoMinimoAtencion
+            else 
+                permanentes << anterior if self.hora - anterior.hora >= TiempoMinimoAtencion
+            end
         end
-        salida
+
+        salida = []
+        reducir(permanentes) do | anterior, actual |
+            next unless actual
+            if salida.count == 0
+                salida << anterior if actual.hora - anterior.hora >= TiempoMinimoObservacion
+            else 
+                salida << actual   if actual.cantidad < anterior.cantidad 
+            end
+        end
+        salida 
     end
 
     def muestra(cantidad=1000)
-        lista = entradas.clone
+        lista = self.entradas.clone
         cantidad.times.map do
             i = rand(lista.count)
             j = rand(lista.count)
             redo if i == j
             promedio(lista[i], lista[j])
-        end.promedio.round(1)
+        end
+    end
+
+    def registrar(cantidad)
+        cantidad += registros.last ? registros.last.cantidad : 0
+        self.registros << { cantidad: cantidad, hora: self.hora } if cantidad >= 0
+    end
+
+    def avanzar(tiempo=1)
+        self.reloj ||= 0
+        self.reloj += tiempo if tiempo > 0
     end
 
     def mostrar(lista, texto='')
-        puts " #{nombre} (#{reloj}s, #{lista.count}) [#{texto}] ".titulo do 
-            lista.each{|e| puts ' %2is > %2i ' % [e.hora, e.cantidad] }
+        puts " #{nombre} (#{self.hora}s, #{lista.count}) [#{texto}] #{configurando? ? :configurando : ''} #{editando? ? :editando : ''} ".titulo do 
+            lista.each{|e| puts ' • %2is > %2i ' % [e.hora, e.cantidad] }
         end
         puts ''
     end
-
+    
+    def reducir(lista)
+        pares = lista.zip(lista[1..-1])
+        pares.each{|anterior, actual| yield( anterior, actual ) }
+    end
 end
 
+# ---
+
 def Cola(nombre, cantidad=0, &bloque)
-    tmp = Cola.new(nombre)
+    tmp = Cola.new( nombre )
+    tmp.avanzar(0)
     cantidad.times{ tmp.entrar }
-    tmp.instance_eval(&bloque)
+    tmp.instance_eval( &bloque )
+    tmp 
 end
 
 if __FILE__ == $0
-    
-    puts "Simulacion".pad(100).error
+    puts ' Simulacion '.pad(100).error
     Cola 'Polo Norte' do 
-        10.times{entrar}
+        10.times{ entrar }
+        salir 
 
         avanzar 20
-        3.times{salir}
+        3.times{ salir }
         
         avanzar 30
         salir 
@@ -101,34 +120,24 @@ if __FILE__ == $0
         salir 
 
         avanzar 8
-        2.times{salir}
+        2.times{ salir }
         
         avanzar 10
         salir 
 
-        2.times{entrar}
+        2.times{ entrar }
 
         avanzar 3
-        2.times{salir}
+        2.times{ salir }
         
         avanzar 10
 
-        mostrar registros,   'Registros Brutos'
-        mostrar permanentes, 'Permanentes'
+        # mostrar registros,   'Registros Brutos'
+        # mostrar permanentes, 'Permanentes'
         mostrar entradas,    'Entradas'
 
         # mostrar entradas,    "Entradas" 
    
-        pp muestra(1000)
-
-        # puts "Promedios" do 
-        #     puts "Loco   10  #{promedio(10)}"
-        #     puts "Loco  100  #{promedio(100)}"
-        #     puts "Loco   1k  #{promedio(1000)}"
-        #     puts "Loco  10k  #{promedio(10000)}"
-        #     puts "Loco 100k  #{promedio(100000)}"
-        #     puts "Loco   1M  #{promedio(1000000)}"
-        # end
+        pp muestra(100).promedio.round(1)
     end
  end
-   
--- prueba.rb
+++ prueba.rb
@@ -1,16 +1,4 @@
-require 'discordrb'
-
-bot = Discordrb::Bot.new token: 'Nzg4MjExNjAwNTg2MDQ3NTU4.X9gNQQ.p_tSsd3hj5pw_jFrFwhTSVqD2PQ'
-
-
-bot.send_message 'franco-bot#5114', "Hola mundo!"
-
-# '
-# # p Discordrb::Channel.new(1,1)
-# # bot.message(with_text: 'Ping!') do |event|
-# #   event.respond 'Pong!'
-# # end
-
-# # bot.chanel
-# # at_exit{bot.stop}
-# # bot.run
+puts a = Time.new
+sleep 5
+puts b = Time.new
+puts b - a
