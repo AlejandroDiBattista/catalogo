a8f23a813418c9046f973eb345d9db39077ba490
Alejandro Di Battista <alejandrodibattista@gmail.com>
2021-03-30 21:00:38 -0300

    Update web.rb

--- web.rb
+++ web.rb
@@ -297,3 +297,6 @@ if __FILE__ == $0
 	correr :bajar_todo
 	# pull
 end
+
+expediente	folios	estado	asunto	observacion	adjuntos
+279-05	51	Recepcionado	Ref. usurpacion de calle, propiedad enclavada en calle publica		
